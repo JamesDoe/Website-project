103183a58f4564dd0a0dfc60a16db0b620e4f3ce
James Doe <jamesdoejr@gmail.com>
2018-10-02 17:04:29 -0400

    started to make js more modular

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-function openPage(pageName, elmnt, color) {
+const config = {
+    tabs: {
+        "Login": {background: "red"},
+        "About": {background: "orange"},
+        "Contact": {background: "blue"}
+    }
+};
+
+// function openPage(pageName, elmnt, color) {
+//     // Hide all elements with class="tabcontent" by default */
+//     let i, tabcontent, tablinks;
+//     tabcontent = document.getElementsByClassName("content");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+
+//     // Remove the background color of all tablinks/buttons
+//     tablinks = document.getElementsByClassName("tablink");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].style.backgroundColor = "";
+//     }
+
+//     // Show the specific tab content
+//     document.getElementById(pageName).style.display = "block";
+
+//     // Add the specific color to the button used to open the tab content
+//     elmnt.style.backgroundColor = color;
+// }
+
+function openPage(element) {
     // Hide all elements with class="tabcontent" by default */
-    let i, tabcontent, tablinks;
+    let i, pageName = element.innerText, color = config.tabs[pageName].background, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -16,7 +45,7 @@ function openPage(pageName, elmnt, color) {
     document.getElementById(pageName).style.display = "block";
 
     // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
+    element.style.backgroundColor = color;
 }
 
 // Get the element with id="defaultOpen" and click on it
