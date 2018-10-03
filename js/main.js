08f638abcda60602fbd6a110c1a05add1eafc716
James Doe <jamesdoejr@gmail.com>
2018-10-03 13:04:41 -0400

    moved nav button creation into dynamic function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 const config = {
     tabs: {
-        "Login": {background: "red"},
+        "Login": {background: "red", default: true},
         "About": {background: "orange"},
         "Contact": {background: "blue"}
     }
 };
 
+// const links = ["Login", "About", "Contact"];
+const nav = document.getElementById("nav");
+// config.tabs.forEach((obj,index) => {
+// TODO: Use map/weak map to ensure element order
+for (let tab in config.tabs) {
+    let b = document.createElement('button'), buttonText = document.createTextNode(tab), tabContainer = document.getElementById('buttonContainer');
+    b.appendChild(buttonText);
+    b.setAttribute("class","tablink");
+    b.setAttribute("onclick","openPage(this)");
+    if (config.tabs[tab].default) b.setAttribute("id","defaultOpen");
+    
+    buttonContainer.appendChild(b);
+    // let a = document.createElement('a'), node = document.createElement("li"), linkText = document.createTextNode(element);
+    // a.appendChild(linkText);
+    // a.href = "http://example.com";
+    // node.appendChild(a);
+    // node.setAttribute("class","nav_link");
+    // nav.appendChild(node);
+// });
+};
 // function openPage(pageName, elmnt, color) {
 //     // Hide all elements with class="tabcontent" by default */
 //     let i, tabcontent, tablinks;
